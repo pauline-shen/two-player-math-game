a27a0fffdce546506bdb2d26ec3374a02be234fc
Pauline Shen <s9633291@gmail.com>
2023-01-26 05:49:48 +0000

    complete game play with appropriate prompts

--- game.rb
+++ game.rb
@@ -6,39 +6,46 @@ class Game
   CORRECT_ANS = "YES! You are correct."
   GOOD_BYE = "Good bye!"
   QUESTION = "Please enter two numbers to create problem for the other player."
-  attr_reader :p1, :p2, :players
+  TURN = "----- NEW TURN -----"
+  OVER = "----- GAME OVER -----"
+  attr_reader :players
   attr_accessor :current
 
   def initialize(p1_id, p2_id)
-    @p1 = Player.new(p1_id)
-    @p2 = Player.new(p2_id)
+    @players = [Player.new(p1_id), Player.new(p2_id)]
     @current = 0
   end
 
   def play_game
-    while (is_alive(p1) && is_alive(p2))
-      current_player = p1
-      answer_player = p2
-      puts QUESTION
+    while (is_alive(players[0]) && is_alive(players[1]))
+      if (current % 2 == 0)
+        current_player, answer_player  = @players[0], @players[1]
+      else
+        current_player, answer_player  = @players[1], @players[0]
+      end
+      puts TURN
+      puts current_player.info + ": " + QUESTION
       num_one = Integer(gets.chomp)
       num_two = Integer(gets.chomp)
       current_problem = Problem.new(num_one, num_two)
-      puts p1.info + ": " + current_problem.question
+      puts answer_player.info + ": " + current_problem.question
       answer = Integer(gets.chomp)
       if (answer != current_problem.ans)
         puts WRONG_ANS
-        if (answer_player.lives - 1 == 0)
+        if ((answer_player.lives - 1) == 0)
           answer_player.lives -= 1
-          puts answer_player.lives
+          display_score()
           break
         end
         answer_player.lives -= 1
       elsif (answer == current_problem.ans)
         puts CORRECT_ANS
       end
+      display_score()
       @current += 1
     end
-    #puts winner
+    winner(current_player)
+    puts OVER
     puts GOOD_BYE
   end
 
@@ -46,11 +53,12 @@ class Game
     player.lives > 0 ? true : false
   end
 
-  # def display_score
-  # end
+  def display_score()
+    puts "P#{players[0].id}: #{players[0].lives}/3 vs P#{players[1].id}: #{players[1].lives}/3"
+  end
 
-  # def winner
-  # end
+  def winner(player)
+    puts "#{player.info} wins with a score of #{player.lives}/3"
+  end
 
-  
 end
--- main.rb
+++ main.rb
@@ -1,11 +1,5 @@
 require 'pp'
 require_relative './game'
-require_relative './problem'
-require_relative './player'
 
 g1 = Game.new(1, 2)
 g1.play_game
-#puts g1.players
-# p = Problem.new(1,2)
-# puts p.question
-# puts p.ans
